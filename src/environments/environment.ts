16120eebbc0c6e3dab60199ffb9262e1d9734557
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-03-17 01:33:13 +0300

    rental component added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:44343/',
+  baseUrl: 'https://localhost:44343/uploads',
   apiUrl: 'https://localhost:44343/api/',
 };
 
